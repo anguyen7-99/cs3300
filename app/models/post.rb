b18b6949dcd7c1f5c392d2f91ca01c0b4145515f
anguyen7-9 <anguyen7@uccs.edu>
2021-05-04 23:37:24 +0000

    add iteration 4

--- app/models/post.rb
+++
@@ -1,9 +0,0 @@
-class Post < ApplicationRecord
-    extend FriendlyId
-    friendly_id :title, use: :slugged
-    
-    def should_generate_new_friendly_id?
-        titel_changed?
-    end
-    
-end
