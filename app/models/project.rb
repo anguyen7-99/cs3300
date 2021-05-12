6a452cfbe098c054583838755f2e96531eae1a45
anguyen7-9 <anguyen7@uccs.edu>
2021-05-12 01:13:51 +0000

    add Iteration 6

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,5 +5,9 @@ class Project < ApplicationRecord
       def should_generate_new_friendly_id?
           title_changed?
       end
+      def display_day_published
+        "#{created_at.strftime('%-b %-d, %Y')}"
+      end
+      
     validates_presence_of :title, :description
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'about' => 'pages#about', as: :about
+  get 'contact' => 'pages#contact', as: :contact
   devise_for :users
   root "projects#index"
   resources :projects
